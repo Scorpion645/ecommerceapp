5ff22785aa6eb2364eecca633f91a8ea3bb49ec3
Scorpion645 <scorpion645@gmail.com>
2022-12-03 16:51:42 -0500

    Singin screen finished

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,76 +10,91 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          //  Email textfield
-          Container(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(12)),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 14.0),
-                  child: TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Email',
-                        contentPadding: EdgeInsets.symmetric(horizontal: 20)),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //  Email textfield
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                    child: TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Email',
+                          contentPadding: EdgeInsets.symmetric(horizontal: 20)),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-
-          SizedBox(
-            height: 20,
-          ),
-
-          //  Password textfield
-          Container(
-            child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(12)),
-                    child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 14.0),
-                        child: TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: 'Password',
-                                contentPadding:
-                                    EdgeInsets.symmetric(horizontal: 20)))))),
-          ),
-
-          SizedBox(
-            height: 20,
-          ),
-
-          // Signin Button
-          Container(
-            child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 100),
-                child: Container(
-                    padding: EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                        color: BTNGreen,
-                        borderRadius: BorderRadius.circular(12)),
-                    child: Center(
-                        child: Text("Sign in",
-                            style: GoogleFonts.robotoCondensed(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18))))),
-          )
-        ],
+    
+            SizedBox(
+              height: 20,
+            ),
+    
+            //  Password textfield
+            Container(
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                          child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: 'Password',
+                                  contentPadding:
+                                      EdgeInsets.symmetric(horizontal: 20)))))),
+            ),
+    
+            SizedBox(
+              height: 20,
+            ),
+    
+            // Signin Button
+            Container(
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 100),
+                  child: Container(
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                          color: BTNGreen,
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Center(
+                          child: Text("Sign in",
+                              style: GoogleFonts.robotoCondensed(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18))))),
+            ),
+    
+            SizedBox(height: 30),
+    
+            // Signup text
+            Container(
+                child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Do not have an account?'),
+                  TextButton(onPressed: () {}, child: Text('Sign up',style: TextStyle(fontWeight: FontWeight.bold),))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
